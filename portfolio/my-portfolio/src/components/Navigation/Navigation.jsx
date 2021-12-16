24d36ab1ec6980d7fc9d18b9d797cc8b11d06365
Abdullah Sahraoui <abd.sahraoui26@gmail.com>
2021-12-16 00:41:31 +0000

    Comments.

--- portfolio/my-portfolio/src/components/Navigation/Navigation.jsx
+++ portfolio/my-portfolio/src/components/Navigation/Navigation.jsx
@@ -2,11 +2,14 @@ import './navigation.css';
 
 const Navigation = () => {
   return (
+    // Name + title
     <div className="header flex shadow-lg sticky">
       <div className="p-4 flex flex-col w-3/5">
         <p className="black-rocket text-7xl ml-2">Abdullah Sahraoui</p>
         <p className="subtitle ml-6">Junior Front-End Developer</p>
       </div>
+
+      {/* Navigation links */}
       <ul className="flex p-2 subtitle uppercase w-2/5 justify-around items-center">
         <li className="transition-opacity hover:opacity-75"><a href="#">Home</a></li>
         <li className="transition-opacity hover:opacity-75"><a href="#">Projects</a></li>
