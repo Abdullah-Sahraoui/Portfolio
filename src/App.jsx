c5500a008bf5ae7889360a08f11869e3f761bfb6
Abdullah Sahraoui <abd.sahraoui26@gmail.com>
2021-12-17 19:26:41 +0000

    Added active and inactive classes for nav.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Navigation from './components/Navigation/Navigation';
+
+function App() {
+  return (
+    <div className="main-wrapper overlay">
+      <Navigation />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import './navigation.css';
+
+const Navigation = () => {
+  const [navbar, setNavbar] = useState(false);
+
+  const changeBg = () => {
+    (window.scrollY >= 127) ? setNavbar(true) : setNavbar(false);
+  }
+
+  window.addEventListener("scroll", changeBg);
+
+  const classes = "header flex shadow-lg sticky";
+  
+  return (
+    <div className={(navbar ? classes + " active" : classes + " inactive")}>
+      <div className="p-4 flex flex-col w-3/5">
+        <p className="black-rocket text-7xl ml-2">Abdullah Sahraoui</p>
+        <p className="subtitle ml-6">Junior Front-End Developer</p>
+      </div>
+      <ul className="flex p-2 subtitle uppercase w-2/5 justify-around items-center">
+        <li className="transition-opacity hover:opacity-75"><a href="#">Home</a></li>
+        <li className="transition-opacity hover:opacity-75"><a href="#">Projects</a></li>
+        <li className="transition-opacity hover:opacity-75"><a href="#">About</a></li>
+        <li className="transition-opacity hover:opacity-75"><a href="#">Contact</a></li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navigation;
